c6fdcedc30a6cfd4578ac6a208241356b703249b
andy-liuu <59021924+andy-liuu@users.noreply.github.com>
2021-07-29 16:23:24 -0400

    js functions
    make event listings + formatted html body and head tags for convention + removed templates

--- index.js
+++ index.js
@@ -32,20 +32,151 @@ async function getEventsPromise(){
             console.log("Fetch promise rejected")
         })
 }
+
+
 async function createEventList(){
     let events = await getEventsPromise() //actual array now
 
-    console.log(events)
+    for(let i = 0; i<events.length; i++){
+        document.getElementById("container-events").appendChild(createOneListing(events[i]));
+    }
+
+}
+
+
+
+
+//icons used in each event
+let timeIcon = document.createElement("img");
+timeIcon.setAttribute("src", "Calendar_Icon.png");
+timeIcon.setAttribute("alt", "Calendar Icon");
+timeIcon.setAttribute("title", "Calendar Icon");
+
+let infoIcon = document.createElement("img");
+infoIcon.setAttribute("src", "Info_Icon.png");
+infoIcon.setAttribute("alt", "Info Icon");
+infoIcon.setAttribute("title", "Info Icon.png");
+
+
+let locationIcon = document.createElement("img");
+locationIcon.setAttribute("src", "Location_Icon.png");
+locationIcon.setAttribute("alt", "Location Icon");
+locationIcon.setAttribute("title", "Location Icon");
+
+;
+
+
+
+
+function createOneListing(data){
+    /*
+    params: json object that represents the info for each
+
+    purpose: return the element with all of the information, to be later put into the html using DOM methods
+                all listings are styled the same i.e. same classes and stuff
+
+    */
+
+
+    //icons used in each event
+    let timeIcon = document.createElement("img");
+    timeIcon.setAttribute("src", "Calendar_Icon.png");
+    timeIcon.setAttribute("alt", "Calendar Icon");
+    timeIcon.setAttribute("title", "Calendar Icon");
+
+    let infoIcon = document.createElement("img");
+    infoIcon.setAttribute("src", "Info_Icon.png");
+    infoIcon.setAttribute("alt", "Info Icon");
+    infoIcon.setAttribute("title", "Info Icon.png");
+
+
+    let locationIcon = document.createElement("img");
+    locationIcon.setAttribute("src", "Location_Icon.png");
+    locationIcon.setAttribute("alt", "Location Icon");
+    locationIcon.setAttribute("title", "Location Icon");
+
+
+
+    //big container containing everything 
+    let outershell = document.createElement("div");
+    outershell.classList.add("event");
+
+    //small container with info about the event
+    let eventcontent = document.createElement("div");
+    eventcontent.classList.add("event-content");
+    
+    //name of event
+    let title = document.createElement("h4");
+    title.innerHTML = data["name"];
+    eventcontent.appendChild(title);
+
+    
+
+    //time frame
+    let dateStart = new Date(data["dateStart"]);
+    let timeStart = new Date(data["timeStart"]);
+    let dateEnd = new Date(data["dateEnd"]);
+    let timeEnd = new Date(data["timeEnd"]);
+
+    let timeframe = document.createElement("p");
+    timeframe.appendChild(timeIcon);
+
+    //for toLocale___String formatting
+    const timeOps = {hour: 'numeric', minute: 'numeric', hour12: true};
+    const dayOps = {year: 'numeric', month: 'long', day: 'numeric'};
+
+    //single date event
+    if (data["dateStart"] === data["dateEnd"] || data["dateEnd"] === ""){
+        timeframe.appendChild(document.createTextNode(
+            ` ${timeStart.toLocaleTimeString("en-US",timeOps)} - ${timeEnd.toLocaleTimeString("en-US",timeOps)} | ${dateStart.toLocaleDateString("en-US", dayOps)}`
+            ));
+    }
+    else{
+        timeframe.appendChild(document.createTextNode(
+            ` ${timeStart.toLocaleTimeString("en-US",timeOps)} ${dateStart.toLocaleDateString("en-US", dayOps)} - ${timeEnd.toLocaleTimeString("en-US",timeOps)} ${dateEnd.toLocaleDateString("en-US", dayOps)}`
+            ));
+    }
+
+    eventcontent.appendChild(timeframe);
+
+
+    //location or link
+    let place = document.createElement("p");
+    place.appendChild(locationIcon);
+
+    //online
+    if (data["isOnline"] === "Online"){
+        place.appendChild(document.createTextNode(` ${data["link"]}`))
+    }
+    else{
+        place.appendChild(document.createTextNode(` ${data["address"]} ${data["city"]}, ${data["province"]}, ${data["postalCode"]}`))
+    }
+
+    eventcontent.appendChild(place);
 
     
 
 
 
 
+    
+
+
+    //small container with visiting button
+    let visitbutton = document.createElement("button");
+    visitbutton.appendChild(document.createTextNode("Visit"));
+    visitbutton.classList.add("visit-button");
+
+
+    outershell.appendChild(eventcontent);
+    outershell.appendChild(visitbutton);
+
+
+    return outershell;
 }
 
 
-createEventList()
+//createEventList()
 
 
 
